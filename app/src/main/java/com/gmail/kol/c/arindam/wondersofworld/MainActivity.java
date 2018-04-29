72a3bb875dbae6beb84f8d03320c1e81f77bf11f
Arindam <arindam.c.kol@gmail.com>
2018-04-29 15:42:44 +0530

    Added dialog boxes. Hope it is complete.

--- app/src/main/java/com/gmail/kol/c/arindam/wondersofworld/MainActivity.java
+++ app/src/main/java/com/gmail/kol/c/arindam/wondersofworld/MainActivity.java
@@ -1,11 +1,18 @@
 package com.gmail.kol.c.arindam.wondersofworld;
 
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
 import android.content.res.Resources;
 import android.content.res.TypedArray;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RadioButton;
@@ -18,50 +25,71 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
+    // identifier for values to save for instance change
     static final String STATE_POSITION = "current_position";
     static final String STATE_HINT = "current_hint";
     static final String STATE_SCORE = "current_score";
     static final String STATE_OPTION = "selected_radio_button";
+    static final String STATE_LOGIN = "login_dialog_shown";
+    static final String STATE_GAMEOVER = "gameover_dialog_shown";
+    static final String STATE_GAMEOVER_MESSAGE = "gameover_message";
+    static final String STATE_PLAYERNAME = "playername";
     static final String STATE_ALPHA_ROW1 = "current_alpha_value_array1";
     static final String STATE_ALPHA_ROW2 = "current_alpha_value_array2";
     static final String STATE_ALPHA_ROW3 = "current_alpha_value_array3";
 
+    private boolean isLoginShown = false;
+    private boolean isGameOver = false;
     private int position = 0; //hold current position of wonder arraylist
     private int hintNumber = -1; // hold current position of hint array of Wonder class
     private int score = 0; // score for quiz
-    private int indexOfSelectedOption = -1;
-    static final float BLURRED = 0.95f; // alpha value for blurred view widget
+    private int indexOfSelectedOption = -1; // radio button index
+    static final float BLURRED = 0.92f; // alpha value for blurred view widget
     static final float TRANSPARENT = 0f; // alpha value for transparent view widget
     // array to store current alpha value of 3X3 view widget
     private float[][] viewTable = {{BLURRED,BLURRED,BLURRED},{BLURRED,BLURRED,BLURRED},{BLURRED,BLURRED,BLURRED}};
+    private String playerName = ""; // player name
+    private String gameoverMessage = "";
+    //variables for views
     private RadioGroup optionRadioGroup;
     private ImageView mImageView;
     private TextView hintTextView;
     private TextView scoreTextView;
+    private TextView instructionTextView;
     private LinearLayout rootLayout;
     private Button hintButton;
     private Button submitButton;
-    private ArrayList <Wonder> wonders = new ArrayList<Wonder>();
+
+    private SharedPreferences mSharedPreferences; // to save player name in app
+    //login dialog box views
+    public EditText nameText;
+    public CheckBox saveUserCheck;
+
+    private ArrayList <Wonder> wonders = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //put data from string arrays to wonder array list
         setWondersData();
-
+        // initialise views
         rootLayout = findViewById(R.id.root_layout);
         optionRadioGroup = findViewById(R.id.options_radio_group);
         hintTextView = findViewById(R.id.hint_textview);
         scoreTextView = findViewById(R.id.score_text);
+        instructionTextView = findViewById(R.id.instruction);
         hintButton = findViewById(R.id.show_hint);
         submitButton = findViewById(R.id.submit_button);
         mImageView = findViewById(R.id.image);
 
-        submitButton.setClickable(false);
-        submitButton.setAlpha(0.5f);
-
+        //if instance changed load data
         if (savedInstanceState != null) {
+            isLoginShown = savedInstanceState.getBoolean(STATE_LOGIN);
+            isGameOver = savedInstanceState.getBoolean(STATE_GAMEOVER);
+            gameoverMessage = savedInstanceState.getString(STATE_GAMEOVER_MESSAGE);
+            playerName = savedInstanceState.getString(STATE_PLAYERNAME);
             position = savedInstanceState.getInt(STATE_POSITION);
             hintNumber = savedInstanceState.getInt(STATE_HINT);
             score = savedInstanceState.getInt(STATE_SCORE);
@@ -69,6 +97,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             viewTable[0] = savedInstanceState.getFloatArray(STATE_ALPHA_ROW1);
             viewTable[1] = savedInstanceState.getFloatArray(STATE_ALPHA_ROW2);
             viewTable[2] = savedInstanceState.getFloatArray(STATE_ALPHA_ROW3);
+
+            if (isGameOver) {
+                gameOverAlert(gameoverMessage);
+            }
 
             if (hintNumber>=8) {
                 hintButton.setClickable(false);
@@ -93,11 +125,23 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             }
         }
 
+        mSharedPreferences = getPreferences(Context.MODE_PRIVATE);
+        boolean isLoginSaved = mSharedPreferences.getBoolean("isLoginSaved", false);
+
+        if (isLoginSaved) {
+            playerName = mSharedPreferences.getString("player_name", "No Name");
+        } else if (isLoginShown == false) {
+            loginDialog();
+        }
+
+        //load options in radio group
         setOption();
 
         mImageView.setImageResource(wonders.get(position).getImageId());
         scoreTextView.setText(getString(R.string.score_text,score));
+        hintTextView.setText(R.string.hintText);
 
+        // handle radio group selection
         optionRadioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(RadioGroup radioGroup, int i) {
@@ -106,11 +150,16 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 submitButton.setAlpha(1f);
             }
         });
+        //attach click listener to views
+        instructionTextView.setOnClickListener(this);
         hintButton.setOnClickListener(this);
         submitButton.setOnClickListener(this);
-
+        //disable submit button
+        submitButton.setClickable(false);
+        submitButton.setAlpha(0.5f);
     }
 
+    //handle button click
     @Override
     public void onClick(View view) {
         switch (view.getId()) {
@@ -122,12 +171,21 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 showHint();
                 break;
 
+            case R.id.instruction :
+                showInstruction();
+                break;
+
             default: break;
         }
     }
 
+    //save data during instance change
     @Override
     protected void onSaveInstanceState(Bundle outState) {
+        outState.putBoolean(STATE_LOGIN,isLoginShown);
+        outState.putBoolean(STATE_GAMEOVER,isGameOver);
+        outState.putString(STATE_GAMEOVER_MESSAGE,gameoverMessage);
+        outState.putString(STATE_PLAYERNAME,playerName);
         outState.putInt(STATE_POSITION, position);
         outState.putInt(STATE_HINT,hintNumber);
         outState.putInt(STATE_SCORE,score);
@@ -139,6 +197,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onSaveInstanceState(outState);
     }
 
+    //show next image & change all the required variables
     public void nextImage () {
         position++;
         if(position>9) {position=0;}
@@ -173,7 +232,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         viewTable[viewRow][viewCol]=TRANSPARENT;
         setChildViewVisibility(viewRow, viewCol,viewTable[viewRow][viewCol]);
         hintTextView.setText(wonders.get(position).getHint(hintNumber));
-
 
         // disable showhint button if all hint are shown
         if (hintNumber>=8) {
@@ -213,19 +271,106 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         for (int i=0; i<4; i++) {
             RadioButton tempRadioButton = (RadioButton) optionRadioGroup.getChildAt(i);
             tempRadioButton.setText(wonders.get(position).getOption(i));
-
         }
     }
 
+    //check answer
     public void checkResult () {
         boolean isCorrect = (wonders.get(position).getOption(indexOfSelectedOption)).equals(wonders.get(position).getName());
         if (isCorrect) {
-            score = score + (10-hintNumber-1);
-            scoreTextView.setText(getString(R.string.score_text,score));
-            nextImage();
-            indexOfSelectedOption=-1;
-            submitButton.setClickable(false);
-            submitButton.setAlpha(0.5f);
-       }
+            score = score + (10 - hintNumber - 1);
+            if (position>=9) {
+                isGameOver = true;
+                gameoverMessage = getString(R.string.quiz_complete, playerName,score);
+                gameOverAlert(gameoverMessage);
+            } else {
+                scoreTextView.setText(getString(R.string.score_text, score));
+                nextImage();
+                indexOfSelectedOption = -1;
+                submitButton.setClickable(false);
+                submitButton.setAlpha(0.5f);
+                Toast.makeText(MainActivity.this , playerName + ", You are correct", Toast.LENGTH_SHORT).show();
+            }
+        }
+        else {
+            isGameOver = true;
+            gameoverMessage = getString(R.string.gameover_text, playerName,position,score);
+            gameOverAlert(gameoverMessage);
+        }
+    }
+
+    //game over dialog box
+    public void gameOverAlert (final String message) {
+        AlertDialog.Builder gameoverBuilder = new AlertDialog.Builder(MainActivity.this);
+        gameoverBuilder.setMessage(message).setCancelable(false);
+        gameoverBuilder.setTitle(R.string.app_name);
+        gameoverBuilder.setPositiveButton(R.string.restart, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                score = 0;
+                scoreTextView.setText(getString(R.string.score_text,score));
+                position = -1;
+                nextImage();
+                indexOfSelectedOption=-1;
+                submitButton.setClickable(false);
+                submitButton.setAlpha(0.5f);
+                isGameOver = false;
+                gameoverMessage = "";
+            }
+        });
+        gameoverBuilder.setNegativeButton(R.string.exit, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                finish();
+            }
+        });
+        AlertDialog gameoverDialog = gameoverBuilder.create();
+        gameoverDialog.show();
+    }
+
+    //login dialog box
+    public void loginDialog () {
+        final AlertDialog.Builder loginBuilder = new AlertDialog.Builder(MainActivity.this);
+        View view = LayoutInflater.from(MainActivity.this).inflate(R.layout.dialog_name,null);
+        nameText = view.findViewById(R.id.username);
+        saveUserCheck =view.findViewById(R.id.save_username);
+        loginBuilder.setView(view).setCancelable(false);
+        loginBuilder.setTitle(getString(R.string.app_name) + " - " + getString(R.string.login_text));
+        loginBuilder.setPositiveButton(R.string.save, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                playerName = nameText.getText().toString().trim();
+                int nameLength = playerName.length();
+                isLoginShown = true;
+
+                if(saveUserCheck.isChecked() && nameLength>0) {
+                    SharedPreferences.Editor mEditor = mSharedPreferences.edit();
+                    mEditor.putBoolean("isLoginSaved", true);
+                    mEditor.putString("player_name", playerName);
+                    mEditor.commit();
+                }
+
+                if (nameLength == 0) {
+                    Toast.makeText(MainActivity.this, "We will proceed without a name", Toast.LENGTH_SHORT ).show();
+                }
+            }
+        });
+        AlertDialog getnameDialog = loginBuilder.create();
+        getnameDialog.show();
+    }
+
+    //dialog box for how to play
+    public void showInstruction () {
+        AlertDialog.Builder instructionBuilder = new AlertDialog.Builder(MainActivity.this);
+        instructionBuilder.setTitle(getString(R.string.app_name) + " - " + getString(R.string.quiz_instruction));
+        instructionBuilder.setMessage(getText(R.string.instruction_message)).setCancelable(false);
+        instructionBuilder.setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+
+            }
+        });
+        AlertDialog instructionDialog = instructionBuilder.create();
+        instructionDialog.show();
     }
 }
